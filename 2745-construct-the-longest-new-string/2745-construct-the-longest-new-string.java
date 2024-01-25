b9fa98e10b6cf323ec77191dee91b2f5cd8f158f
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-01-25 22:15:07 +0530

    Time: 1 ms (100.00%), Space: 41.3 MB (55.75%) - LeetHub

---
+++ 2745-construct-the-longest-new-string/2745-construct-the-longest-new-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    public int longestString(int x, int y, int z) {
+        int ans = 0;
+        
+        int AAStart = 0;
+        int BBStart = 0;
+        
+        int x1=x, y1=y;
+        
+        while(x1>0 && y1>0) {
+            AAStart += 2;
+            BBStart += 2;
+            x1--;
+            y1--;
+        }
+        
+        if(x1 > 0) {
+            AAStart++;
+        }
+        
+        if(y1 > 0) {
+            BBStart++;
+        }
+        
+        ans = z + Math.max(AAStart, BBStart);
+        
+        return 2*ans;
+    }
+    
+}
+
