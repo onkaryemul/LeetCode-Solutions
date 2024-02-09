e5f71e2a848604c5af17ef36d5ad3d1ac83106f2
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-02-09 20:39:06 +0530

    Time: 28 ms (20.12%), Space: 43.2 MB (28.83%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,57 +1,43 @@
 class Solution {
     
-    // helper function -> longest increasing subsequence
-    // returns the maximum possible largest divisible subset size
-    private int getLDSSize(int[] nums, int[] dp) {
-        int ldsSize = 1;
-        int n = nums.length;
-        
-        // Longest Increasing Subsequence(LIS) -> O(n^2)
-        for(int i=1; i<n; i++) {
-            for(int j=0; j<i; j++) {
-                if(nums[i] % nums[j] == 0) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    ldsSize = Math.max(ldsSize, dp[i]);
-                }
-            }
-        }
-        
-        return ldsSize;
-    }
+    List<Integer> result;
+    int[] subsetSizeMemo;
     
     
-    // helper function -> return a list or subset of longest increasing subset
-    private List<Integer> constructLDS(int[] nums, int[] dp, int ldsSize) {
-        LinkedList<Integer> lds = new LinkedList<>();
-        
-        int prev = -1;
-        int dpLength = dp.length;
-        
-        for(int i=dpLength-1; i>=0; i--) {
-            if(dp[i] == ldsSize && (prev == -1 || prev % nums[i] == 0)) {
-                lds.addFirst(nums[i]);
-                ldsSize--;
-                prev = nums[i];
-            }
-        }
-        
-        
-        return lds;
-    }
-    
-    
-    // Main algorithm code
+    // Main algorithm code: TC : O(2^n) & SC : O(n)
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
-        
         Arrays.sort(nums);
         
-        int ldsSize = getLDSSize(nums, dp); // maximum possible largest Divisible Subset size
+        subsetSizeMemo = new int[nums.length];
+        Arrays.fill(subsetSizeMemo, -1);
         
-        return constructLDS(nums, dp, ldsSize);
+        result = new ArrayList<>();
+        
+        helper(nums, /* start Index */0, new ArrayList<>(), /* prev */1);
+        
+        return result;
+    }
+    
+    
+    private void helper(int[] nums, int index, List<Integer> currList, int prev) {
+        // Base condition
+        if(currList.size() > result.size()) {
+            result = new ArrayList<>(currList);
+        }
+        
+        for(int i=index; i < nums.length; i++) {
+            
+            if(currList.size() > subsetSizeMemo[i] && nums[i] % prev == 0) {
+                subsetSizeMemo[i] = currList.size();
+                
+                currList.add(nums[i]);
+                
+                helper(nums, i+1, currList, nums[i]);  // recursive call
+    
+                currList.remove(currList.size() - 1); // backtracking            
+            }
+        }
     }
     
 }
-
 
