7a5ffc185bb34921221079b1e92d7dc6f8da19eb
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-03-06 20:06:47 +0530

    Time: 0 ms (100.00%), Space: 44 MB (93.76%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+
+// Floyd Cycle detection Algorithm
+public class Solution {
+    
+    // TC : O(n) & SC : O(1)
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+}
+
