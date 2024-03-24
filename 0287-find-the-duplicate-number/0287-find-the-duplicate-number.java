da3db1de8d14a265c996c1097c1b4e27645084fe
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-03-24 14:20:49 +0530

    Time: 4 ms (85.61%), Space: 58.5 MB (29.21%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    
+    // TC : O(n)
+    // SC : O(1)
+    public int findDuplicate(int[] nums) {
+        // Floyd cycle detection
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        
+        fast = nums[0];
+        
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow; // duplicate number
+    }
+    
+}
+
