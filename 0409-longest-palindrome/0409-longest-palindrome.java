2e949c658d377e708fd37eba7fa74bec5e1138ae
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-06-04 11:31:03 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (46.48%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    // TC : O(n)
+    // SC : O(1)
+    
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];
+        for(char ch : s.toCharArray()) {
+            count[ch]++;
+        }
+        
+        int res = 0;
+        
+        for(int i=65; i < 128; i++) {
+            int val = count[i];
+            
+            res += (val/2)*2;
+            
+            if(res%2==0 && val%2==1) {
+                res++;
+            }
+        }
+        
+        return res;
+    }
+    
+}
+
