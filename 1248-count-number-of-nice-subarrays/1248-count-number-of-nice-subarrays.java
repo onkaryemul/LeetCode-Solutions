bf73a59d57dda9d2a6cdeac6f9271ddbe5eb9d30
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-06-22 09:26:10 +0530

    Time: 14 ms (37.59%), Space: 55.1 MB (36.41%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,36 +1,42 @@
 class Solution {
     
-    // helper function
-    private int atmostKSubarrays(int[] nums, int n, int k) {
+    // TC : O(n)
+    // SC : O(1)
+    
+    
+    // Sliding Window & Two pointer Approach
+    public int numberOfSubarrays(int[] nums, int k) {
+        // Number of subarrays with atmost k odd numbers 
+        // -
+        // Number of subarrays with atmost k-1 odd numbers
+        // = Number of subarrays with exactly k odd numbers
+        
+        return atmostKSubarrays(nums, k) - atmostKSubarrays(nums, k-1);
+    }  
+    
+    
+    private int atmostKSubarrays(int[] nums, int k) {
         int ans = 0;
         
         int count = 0;
         int start = 0;
         
-        for(int end=0; end<n; end++) {
+        for(int end=0; end < nums.length; end++) {
             // Check if odd
             if(nums[end]%2 != 0) {
                 count++;
             }
             
-            while(start<=end && count > k) {
+            while(start <= end && count > k) { 
                 if(nums[start++]%2 != 0) {
                     count--;
                 }
             }
             
-            ans += (end-start+1);
+            ans += (end - start + 1); // add window size 
         }
         
         return ans;
-    }
-    
-    
-    // Main algorithm code    
-    public int numberOfSubarrays(int[] nums, int k) {
-        int n = nums.length;
-        
-        return atmostKSubarrays(nums, n, k) - atmostKSubarrays(nums, n, k-1); // return exactly k number of subarrays which are nice subarrys
     }
     
 }
