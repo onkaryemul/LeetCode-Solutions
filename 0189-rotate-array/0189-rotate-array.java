7bebf85096cbe269311b584cda1b5d4801f9c422
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-06-25 10:41:05 +0530

    Time: 2 ms (29.33%), Space: 57 MB (78.19%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    // TC : O(n)
+    // SC : O(1)
+        
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        
+        k = k % n;
+        if(k == 0) {
+            return;
+        }
+        
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+    }
+    
+    
+    private void reverse(int[] nums, int start, int end) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            
+            start++;
+            end--;
+        }
+    }
+    
+}
+
