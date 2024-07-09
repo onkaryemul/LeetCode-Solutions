b53c4ff18f54050f7559525aaae72523f2eb97f0
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-07-09 11:20:33 +0530

    Time: 3 ms (93.06%), Space: 72.7 MB (58.37%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.java
@@ -0,0 +1,33 @@
+class Solution {
+    
+    // TC : O(n)
+    // SC : O(1)
+    
+    // Using Simple Simulation 
+    public double averageWaitingTime(int[][] customers) {
+        int n = customers.length;
+        
+        double totalWaitTime = 0;
+        
+        int currTime = 0;
+        
+        for(int[] customer : customers) {
+            int arrivalTime = customer[0];
+            int cookTime = customer[1];
+            
+            if(currTime < arrivalTime) {
+                currTime = arrivalTime;
+            }
+            
+            int waitTime = currTime + cookTime - arrivalTime;
+            
+            totalWaitTime += waitTime;
+            
+            currTime += cookTime;
+        }
+        
+        return totalWaitTime / n; // avg waiting time 
+    }
+    
+}
+
