d7c706d7476de61f9ff95b9cb347efc8ef8ebeca
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-07-21 21:15:38 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (99.05%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,44 @@
+class Solution {
+    
+    // TC : O(logn)
+    // SC : O(logn), due to recursive stack space
+    
+    public double myPow(double x, int n) {
+        return solve(x, (long)n);
+    }
+    
+    
+    private double solve(double x, long n) {
+        // Base case
+        if(n == 0) {
+            return 1;
+        }
+        
+        if(n < 0) {
+            return solve(1/x, -n);
+        }
+        
+        if(n%2 == 0) {
+            return solve(x*x, n/2);
+        }
+        
+        return x * solve(x*x, (n-1)/2);
+    }
+    
+}
+
+
+/* 
+Integer contains 32 bits, 1 bit is used for a sign flag (0 is + and 1 is -) and remaining 31 bits for values.
+
+For negative values we can express 2^31 different values (from -1 to -2^31) and for positive values it is one value less since we still need to express zero.
+
+So, for positive we can express 2^31-1 values with 31 bits, from 1 to 2^31-1. 
+That's why max negative and max positive do not match.
+
+So, if n=-2147483648 and we multiply n*-1, 
+We will get a number that is greater than Integer.MAX_VALUE as integer range is from -2147483648 to 2147483647
+
+That is why, we convert it to long.
+*/
+
