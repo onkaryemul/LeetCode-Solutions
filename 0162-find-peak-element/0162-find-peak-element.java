77304d9d5aabb21a2becd6be2c515f7ca4cb9c7e
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-07-24 23:39:41 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (76.84%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,25 @@
+class Solution {
+    
+    // TC : O(logn)
+    // SC : O(1)
+    
+    // Binary Search technique --> on an unsorted array (special case)
+    public int findPeakElement(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        
+        while(low < high) {
+            int mid = low + (high - low)/2;
+            
+            if(nums[mid] < nums[mid+1]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid;
+            }
+        }
+        
+        return low;
+    }
+    
+}
+
