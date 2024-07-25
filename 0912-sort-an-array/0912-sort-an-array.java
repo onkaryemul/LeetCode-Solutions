a3981d52ce45568c5e41f99aa54d04642519a509
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-07-25 10:08:14 +0530

    Time: 53 ms (24.99%), Space: 55.2 MB (94.31%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,43 @@
+class Solution {
+    
+    // TC : O(n + k)
+    // SC : O(n)
+    
+    // Counting Sort
+    public int[] sortArray(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        int min = nums[0], max = nums[0];
+        
+        for(int i=0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            }
+            else {
+                map.put(nums[i], 1);
+            }
+            
+            if(nums[i] < min) {
+                min = nums[i];
+            }
+            
+            if(nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        
+        int index = 0;
+        
+        for(int i=min; i <= max; i++) {
+            while(map.getOrDefault(i, 0) > 0) {
+                nums[index] = i;
+                index++;
+                map.put(i, map.get(i) - 1);
+            }
+        }
+        
+        return nums;
+    }
+    
+}
+
