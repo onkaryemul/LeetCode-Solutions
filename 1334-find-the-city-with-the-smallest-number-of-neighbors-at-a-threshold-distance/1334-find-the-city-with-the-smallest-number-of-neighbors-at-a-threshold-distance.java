3717630e87925af116e88a2b2e23c3ddc2d0eae2
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-07-26 11:12:32 +0530

    Time: 10 ms (51.92%), Space: 43.5 MB (99.13%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,47 +1,69 @@
 class Solution {
     
-  public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-    int ans = -1;
-    int minCitiesCount = n;
-    int[][] dist = floydWarshall(n, edges, distanceThreshold);
-
-    for (int i = 0; i < n; ++i) {
-      int citiesCount = 0;
-      for (int j = 0; j < n; ++j)
-        if (dist[i][j] <= distanceThreshold)
-          ++citiesCount;
-      if (citiesCount <= minCitiesCount) {
-        ans = i;
-        minCitiesCount = citiesCount;
-      }
-    }
-
-    return ans;
-  }
-
+    // TC : O(n^3)
+    // SC : O(n^2)
     
-  private int[][] floydWarshall(int n, int[][] edges, int distanceThreshold) {
-    int[][] dist = new int[n][n];
-    Arrays.stream(dist).forEach(A -> Arrays.fill(A, distanceThreshold + 1));
-
-    for (int i = 0; i < n; ++i)
-      dist[i][i] = 0;
-
-    for (int[] edge : edges) {
-      final int u = edge[0];
-      final int v = edge[1];
-      final int w = edge[2];
-      dist[u][v] = w;
-      dist[v][u] = w;
+    // Floyd Warshall Algorithm
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int cityNo = -1;
+        int minCitiesCnt = n;
+        
+        int[][] dist = floydWarshall(n, edges, distanceThreshold);
+        
+        for(int city=0; city < n; city++) {
+            int cnt = 0;
+            
+            for(int adjCity=0; adjCity < n; adjCity++) {
+                if(dist[city][adjCity] <= distanceThreshold) {
+                    cnt++;
+                }
+            }
+            
+            if(cnt <= minCitiesCnt) {
+                minCitiesCnt = cnt;
+                cityNo = city;
+            }
+        }
+        
+        return cityNo;
     }
 
-    for (int k = 0; k < n; ++k)
-      for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-          dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
 
-    return dist;
-  }
+    private int[][] floydWarshall(int n, int[][] edges, int distanceThreshold) {
+        int[][] dist = new int[n][n];
+        
+        for(int i=0; i < n; i++) {
+            for(int j=0; j < n; j++) {
+                if(i == j) {
+                    dist[i][j] = 0;
+                }
+                else {
+                    dist[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+        
+        for(int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+            
+            dist[u][v] = wt;
+            dist[v][u] = wt;
+        }
+        
+        for(int k=0; k < n; k++) {
+            for(int i=0; i < n; i++) {
+                for(int j=0; j < n; j++) {
+                    if(dist[i][k] == Integer.MAX_VALUE ||  dist[k][j] == Integer.MAX_VALUE) {
+                        continue;
+                    }
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+        
+        return dist;
+    }
     
 }
-
