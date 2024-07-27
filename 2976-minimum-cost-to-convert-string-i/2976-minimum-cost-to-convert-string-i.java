57039d5fc380188269c99e19bff328f29394a762
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-07-27 11:22:54 +0530

    Time: 23 ms (69.27%), Space: 46 MB (14.68%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
@@ -1,62 +1,61 @@
-//T.C : O(n^3)  -> because of nested for loop 26*26*26
-//S.C : O(1) -> We take distances matrix of 26*26 which is constant
-
 class Solution {
     
-    // helper function 
-    private void floydWarshall(long[][] distanceMatrix, char[] original, char[] changed, int[] cost) {
-        int len = original.length;
-        
-        for(int i=0; i<len; i++) {
-            int sourceIdx = original[i] - 'a';
-            int targetIdx = changed[i] - 'a';
-            distanceMatrix[sourceIdx][targetIdx] = Math.min(distanceMatrix[sourceIdx][targetIdx], (long)cost[i]);
-        }
-        
-        // O(n^3) -> nested for loops
-        for(int k=0; k<26; k++) { // intermediate vertices
-            for(int i=0; i<26; i++) {
-                for(int j=0; j<26; j++) {
-                    distanceMatrix[i][j] = Math.min(distanceMatrix[i][j], distanceMatrix[i][k] + distanceMatrix[k][j]);
-                }
-            }
-        }
-    }
+    // TC : O(n + 26*26*26) ~ O(n), due to nested for loops 26*26*26
+    // SC : O(26*26) ~ O(1), we take distance matrix of 26*26 which is constant
     
-    
-    
-    //Approach-1 (Using Simple Floyd Warshall Algorithm)
+    // Using Floyd Warshall Algorithm
     public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
-        long ans = 0; // store the minimum cost to convert the string source to the string target
+        long minCost = 0; // store the minimum cost to convert the source string to the target string
         
         long[][] distanceMatrix = new long[26][26];
-        for(int i=0; i<26; i++) {
+        for(int i=0; i < 26; i++) {
             Arrays.fill(distanceMatrix[i], Integer.MAX_VALUE);
         }
         
         floydWarshall(distanceMatrix, original, changed, cost);
         
-        int len = source.length();
-        for(int i=0; i<len; i++) {
-            char sourceChar = source.charAt(i);
+        int len = source.length(); 
+        for(int i=0; i < len; i++) {
+            char srcChar = source.charAt(i);
             char targetChar = target.charAt(i);
             
-            if( sourceChar == targetChar ) {
+            if(srcChar == targetChar) {
                 continue;
             }
             
-            // If it is impossible to convert source to target, return -1.
-            if(distanceMatrix[sourceChar-'a'][targetChar-'a'] == Integer.MAX_VALUE) {
+            // Check if it is impossible to convert source string to target string, then return -1
+            if(distanceMatrix[srcChar - 'a'][targetChar - 'a'] == Integer.MAX_VALUE) {
                 return -1;
-            } else {
-                ans += distanceMatrix[sourceChar-'a'][targetChar-'a'];
+            }
+            
+            minCost = minCost + distanceMatrix[srcChar - 'a'][targetChar - 'a'];
+        }
+        
+        return minCost;
+    }
+    
+    
+    private void floydWarshall(long[][] distanceMatrix, char[] original, char[] changed, int[] cost) {
+        int n = original.length; // = changed.length  or cost.length
+        
+        // Populate the distance matrix with values of cost[i] from original[i] to changed[i]
+        for(int i=0; i < n; i++) {
+            int srcIdx = original[i] - 'a';
+            int targetIdx = changed[i] - 'a';
+            
+            distanceMatrix[srcIdx][targetIdx] = Math.min(distanceMatrix[srcIdx][targetIdx], (long)cost[i]);
+        }
+        
+        // Perform Floyd Warshall's algorithm to find shortest path of visited one node from other node in O(1) time complexity
+        // O(26*26*26) -> nested for loops
+        for(int k=0; k < 26; k++) { // intermediate vertices
+            for(int i=0; i < 26; i++) {
+                for(int j=0; j < 26; j++) {
+                    distanceMatrix[i][j] = Math.min(distanceMatrix[i][j], distanceMatrix[i][k] + distanceMatrix[k][j]);
+                }
             }
         }
         
-        
-        return ans;
     }
     
 }
-
-
