75311858f13620c4ab4447b91726cdbc9aa0ec14
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-08-15 11:53:22 +0530

    Time: 2 ms (95.27%), Space: 56.1 MB (27.95%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -3,49 +3,42 @@ class Solution {
     // TC : O(n)
     // SC : O(1)
     
+    // Simple Simulation
+    
     // Function to find whether each customer can be provided with correct change
     public boolean lemonadeChange(int[] bills) {
-        int n = bills.length; // no. of customers
+        int n = bills.length;
         
-        int five = 0; // counter for $5 bills
-        int ten = 0; // counter for $10 bills
+        int five = 0;
+        int ten = 0;
         
-        for(int i=0; i < n; i++) { // iterate through each customer's bill
-            // 1. If the customer's bill is $5
-            if(bills[i] == 5) {
-                five++; // increment the count of $5 bills
+        for(int bill : bills) {
+            if(bill == 5) {
+                five++;
             }
-            // 2. If the customer's bill is $10
-            else if(bills[i] == 10) {
-                // Check if there are $5 bills available to given change
+            else if(bill == 10) {
                 if(five > 0) {
-                    five--; // use one $5 bill to give change
-                    ten++; // receive one $10 bill
+                    five--;
+                    ten++;
                 }
-                else { // if no $5 bill available, then return false
-                    return false; 
-                }
-            }
-            // 3. If the customer's bill is $20
-            else {
-                // Check if there are both $5 and $10 bills availabel to give change
-                if(five > 0 && ten > 0) {
-                    five--; // use one $5 bill
-                    ten--; // use one $10 bill
-                }
-                // If there are not enough $10 bills,
-                // Check if there are at least three $5 bills available
-                else if (five >= 3) {
-                    five -= 3; // use three $5 bills to give change
-                } 
-                else { // if unable to give change, return false
+                else {
                     return false;
                 }
             }
-        
+            else { // bill == 20
+                if(five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                }
+                else if(five >= 3) {
+                    five -= 3;
+                }
+                else {
+                    return false;
+                }
+            }
         }
         
-        // Return true, if all customers are served with correct change
         return true;
     }
     
