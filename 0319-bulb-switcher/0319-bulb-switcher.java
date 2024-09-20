6626d6da7c73c67b93602373f3a715c3757e4de5
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-09-21 00:12:09 +0530

    Time: 0 ms (100.00%), Space: 40 MB (92.25%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -0,0 +1,13 @@
+class Solution {
+    
+    // TC : O(sqrt(n))
+    // SC : O(1)
+    
+    // Finding "count" of "perfect square numbers from 1 to n" (ie. 1, 4, 9, 16 ... n)
+     
+    public int bulbSwitch(int n) {
+         return (int)Math.sqrt(n);
+    }
+    
+}
+
