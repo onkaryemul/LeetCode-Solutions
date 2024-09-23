64c83a2073c78437c66e69f5ea460477272e6f09
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-09-23 19:29:46 +0530

    Time: 42 ms (68.71%), Space: 45.5 MB (21.43%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -1,57 +1,55 @@
-// Recursion + Memoization
 class Solution {
     
-    int n;
-    int[] t;
+    // TC : O(n^3)
+    // SC : O(total no. of characters in dictionary words  +  n for memoization array)
     
-    // helper function -> recursion & dp (memoization)
-    private int solve(int idx, String s, Set<String> set) {
+    // Using Recursion + Memoization
+    
+    // function to return the "minimum no. of characters left over" if you break up s optimally
+    public int minExtraChar(String s, String[] dictionary) {
+        int n = s.length();
+        
+        HashSet<String> set = new HashSet<>();
+        for(String word : dictionary) {
+            set.add(word);
+        }
+        
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        
+        return solve(0, n, s, set, dp); // calling helper function
+    }
+    
+    
+    // helper function -> recursion + memoization 
+    // Dynamic Programming (DP) --> Take - Not take
+    private int solve(int i, int n, String s, HashSet<String> set, int[] dp) {
         // Base case
-        if(idx >= n) {
+        if(i >= n) {
             return 0;
         }
         
-        // Memoization
-        if(t[idx] != -1) {
-            return t[idx];
+        // Check if it is already computed
+        if(dp[i] != -1) {
+            return dp[i];
         }
         
+        // Not Take --> means adding 1 extra character(left over)
+        int result = 1 + solve(i+1, n, s, set, dp); // skippping the ith character
         
-        int minChars = n;
-        
-        
-        for(int i=idx; i<n; i++) {
-            String temp = s.substring(idx, i+1);
+        // Take --> means adding 0 extra characters(left over)
+        // taking substring starting from i to j which is present in set
+        for(int j=i; j < n; j++) {
+            String curr = s.substring(i, j+1);
             
-            if(set.contains(temp)) {
-                minChars = Math.min(minChars, solve(i+1, s, set));
-            } else {
-                minChars = Math.min(minChars, temp.length() + solve(i+1, s, set));
+            if(set.contains(curr)) {
+                // valid substring
+                result = Math.min(result, solve(j+1, n, s, set, dp));
             }
         }
         
-        
-        return t[idx] = minChars;
-    }
-    
-    
-    // Main algorithm code
-    public int minExtraChar(String s, String[] dictionary) {
-        n = s.length();
-        
-        t = new int[n];
-        Arrays.fill(t, -1);
-        
-        Set<String> set = new HashSet<String>();
-        
-        for(String str : dictionary) {
-            set.add(str);
-        }
-        
-        
-        return solve(0, s, set);
+        return dp[i] = result;
     }
     
 }
-
 
