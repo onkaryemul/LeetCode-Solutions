f576c40210f7d06f6af3c0ab566c2633c18f9251
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-12-04 13:06:45 +0530

    Time: 7 ms (43.44%), Space: 45.4 MB (68.42%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.java
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.java
@@ -6,35 +6,28 @@ class Solution {
     // Greedy approach
     // Try to find match so early as possible
     
+    //Approach (Simple traversal using two pointers)
+    
     public boolean canMakeSubsequence(String str1, String str2) {
         int m = str1.length();
         int n = str2.length();
         
-        if (n > m) 
-           return false;
-
-        int first = -1, second = -1;
-        
-        while (first < m && second < n) {
-            first++;
-            second++;
-            
-            // Find matching char with diff <= 1
-            while (first < m) {
-                char nextChar = (char) (((str1.charAt(first) - 'a' + 1) % 26) + 'a');
-                
-                if (second < n && (str2.charAt(second) == str1.charAt(first) || str2.charAt(second) == nextChar)) {
-                    break;
-                }
-                
-                first++;
-            }
+        if (n > m) {
+            return false;
         }
         
-        if (first == m && second < n)
-            return false;
+        int i = 0;
+        int j = 0;
         
-        return true;
+        while (i < m && j < n) {
+            if ((str1.charAt(i) == str2.charAt(j)) || (str1.charAt(i) + 1 == str2.charAt(j)) || (str1.charAt(i) - 25 == str2.charAt(j))) {
+                j++;
+            }
+            
+            i++;
+        }
+        
+        return j == n;
     }
     
 }
