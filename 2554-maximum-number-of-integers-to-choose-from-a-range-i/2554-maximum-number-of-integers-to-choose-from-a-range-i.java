291c29697642b81f113c5fdf0f00456ca1a936d7
Onkar Yemul <110020317+onkaryemul@users.noreply.github.com>
2024-12-06 21:03:42 +0530

    Time: 47 ms (59.78%), Space: 45.9 MB (46.36%) - LeetHub

---
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+    // T.C : O(n)
+    // S.C : O(m)
+
+    // Approach - Simple Greedily select smallest numbers
+    
+    public int maxCount(int[] banned, int n, int maxSum) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int num : banned) {
+            set.add(num);
+        }
+
+        int count = 0;
+        int sum = 0;
+
+        for (int num = 1; num <= n; num++) {
+            if (set.contains(num)) {
+                continue;
+            }
+
+            if (sum + num <= maxSum) {
+                count++;
+                sum += num;
+            } else {
+                break;
+            }
+        }
+
+        return count;
+    }
+    
+}
+
